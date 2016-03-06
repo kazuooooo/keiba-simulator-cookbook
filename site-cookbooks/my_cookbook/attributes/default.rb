611f1134e188187bb13dbb5f0ebf4142d7342744
kazuooooo <matsumotokazuya7@gmail.com>
2016-03-07 01:11:02 +0900

    mysql_recipe add

--- site-cookbooks/my_cookbook/attributes/default.rb
+++ site-cookbooks/my_cookbook/attributes/default.rb
@@ -2,3 +2,10 @@ node.override[:rbenv_install_rubies][:global_version] = '2.2.4'
 node.override[:rbenv_install_rubies][:gems] = ['bundler', 'rbenv-rehash', 'pry']
 node.override[:rbenv_install_rubies][:other_versions] = []
 node.override[:rbenv_install_rubies][:lib_packages] = []
+# mysql
+node.default["staging_cookbook"]["server_name"] = "vagrant.local"
+node.default["mysql"]["staging_db_name"] = "db_staging"
+node.default["mysql"]["staging_user_name"] = "user_staging"
+node.default["mysql"]["staging_password"] = "pass_staging"
+node.default["mysql"]["user"] = "vagrant"
+node.default["mysql"]["group"] = "vagrant"
---
+++ site-cookbooks/my_cookbook/recipes/mysql_recipe.rb
@@ -0,0 +1,40 @@
+# 最初のpackageブロックでmysqlをインストール
+%w(mysql-client mysql-server).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+# templateブロックでDababase作成sqlとユーザー作成sqlをサーバの所定のディレクトリにコピーする
+template "create database sql" do
+  path "/home/#{node['mysql']['user']}/create_databases.sql"
+  source "create_databases.sql.erb"
+  owner node['mysql']['user']
+  group node['mysql']['group']
+  mode "0644"
+end
+
+template "create users sql" do
+  path "/home/#{node['mysql']['user']}/create_users.sql"
+  source "create_users.sql.erb"
+  owner node['mysql']['user']
+  group node['mysql']['group']
+  mode "0644"
+end
+
+# executeブロックで上記のコピーしたsqlファイルを実行
+execute "exec create databases sql" do
+  command "mysql -u root --password='#{node['mysql']['server_root_password']}' < /home/#{node['mysql']['user']}/create_databases.sql"
+  not_if "mysql -u root -p#{node['mysql']['server_root_password']} -D #{node['mysql']['staging_db_name']}"
+  user node['mysql']['user']
+  group node['mysql']['group']
+  environment 'HOME' => "/home/#{node['mysql']['user']}"
+end
+
+execute "exec create users sql" do
+  command "mysql -u root --password='#{node['mysql']['server_root_password']}' < /home/#{node['mysql']['user']}/create_users.sql"
+  not_if "mysql -u #{node['mysql']['staging_user_name']} -p#{node['mysql']['staging_password']}"
+  user node['mysql']['user']
+  group node['mysql']['group']
+  environment 'HOME' => "/home/#{node['mysql']['user']}"
+end
